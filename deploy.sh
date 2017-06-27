9b892f5c867ea17026b65cb88531dbc4edcf352e
Mike Flynn <spikeon@gmail.com>
2017-06-27 00:36:08 -0700

    * Testing Junk in the Trunk

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 # Orig Source : https://gist.github.com/BFTrick/3767319
 # Modified by mflynn to allow for unstable releases
+# Modified to work with a build directory
+# Modified to work with
 
 # A modification of Dean Clatworthy's deploy script as found here: https://github.com/deanc/wordpress-plugin-git-svn
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
@@ -14,54 +16,57 @@ MAINFILE="include.php"                              # this should be the name of
 # git config
 GITPATH="$CURRENTDIR"                              # this file should be in the base of your git repository
 
+BUILDPATH="$GITPATH/build/"
+DISTPATH="$GITPATH/dist/"
+
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG"                          # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/include/"  # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="mflynn"                                    # your svn username
 
+while [[ -z "$CHANGELOG" ]]; do
+    printf '%s\n' '' '# Changes:' '# Example:' '# * added changelog' > /tmp/deploy_script_changelog.txt
+    $EDITOR /tmp/deploy_script_changelog.txt
+    CHANGELOG=`grep "^[^#]" /tmp/deploy_script_changelog.txt`
+done
 
-# Let's begin...
-echo ".........................................."
-echo
-echo "Preparing to deploy wordpress plugin"
-echo
-echo ".........................................."
-echo
+COMMITMSG=$CHANGELOG
 
-# Check version in readme.txt is the same as plugin file
-STABLE=`grep "^Stable tag" $GITPATH/readme.txt | awk -F' ' '{print $3}'`
-VERSION=`grep "^ \* Version" $GITPATH/$MAINFILE | awk -F' ' '{print $3}'`
+printf '%s\n' '' '# Changes:' '# Example:' '# * Fixed that major bug guys, sorry'  > /tmp/deploy_script_upgrade.txt
+$EDITOR /tmp/deploy_script_upgrade.txt
+UPGRADENOTICE=`grep "^[^#]" /tmp/deploy_script_upgrade.txt`
+
+versiony package.json --patch
+
+grunt copy:build
+
+STABLE=`grep "^Stable tag" $BUILDPATH/readme.txt | awk -F' ' '{print $3}'`
+VERSION=`grep "^ \* Version" $BUILDPATH/$MAINFILE | awk -F' ' '{print $3}'`
+
+printf '%s\n' "= $VERSION =" "$CHANGELOG" '' "`cat CHANGELOG.md`" > CHANGELOG.md
+
+if [[ ! -z "$UPGRADENOTICE" ]]; then
+printf '%s\n' "= $VERSION =" "$UPGRADENOTICE" '' "`cat UPGRADE_NOTICE.md`" > UPGRADE_NOTICE.md
+fi
 
 echo "Stable: $STABLE"
 echo "$MAINFILE version: $VERSION"
 
-if [ "$VERSION" != "$STABLE" ]; then
-    echo "Notice: You are submitting a version newer than the stable version."
-    else
-    echo "Stable Release";
-fi
+grunt copy:dist
+grunt copy:assets
 
-cd $GITPATH
-
-git add .
-
-echo -e "Enter a commit message for this new version: \c"
-read COMMITMSG
 git commit -am "$COMMITMSG"
 
 echo "Tagging new version in git"
 git tag -a "$VERSION" -m "Tagging version $VERSION"
 
-echo "Pushing latest commit to origin, with tags"
-git push origin master
-git push origin master --tags
+echo "Pushing latest commit to portfolio, with tags"
+git push portfolio master
+git push portfilio master --tags
 
 echo
 echo "Creating local copy of SVN repo ..."
 svn co $SVNURL $SVNPATH
-
-echo "Exporting the HEAD of master from git to the trunk of SVN"
-git checkout-index -a -f --prefix=$SVNPATH/trunk/
 
 echo "Ignoring github specific & deployment script"
 svn propset svn:ignore "deploy.sh
@@ -69,14 +74,24 @@ README.md
 .git
 .gitignore" "$SVNPATH/trunk/"
 
-echo "Moving assets-wp-repo"
+echo "Changing directory to SVN"
+cd $SVNPATH/trunk/
+
+rm -rf *
+
+cp $DISTPATH/* .
+
+ls -la
+
+rm -fr $SVNPATH/
+exit;
+
+echo "Moving assets"
 mkdir $SVNPATH/assets/
 mv $SVNPATH/trunk/assets/* $SVNPATH/assets/
 svn add $SVNPATH/assets/
 svn delete $SVNPATH/trunk/assets
 
-echo "Changing directory to SVN"
-cd $SVNPATH/trunk/
 # Add all new files that are not set to be ignored
 svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
 echo "committing to trunk"
