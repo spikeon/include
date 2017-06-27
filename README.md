23d853678c3199db9e71de8820db6465c260f4c8
Mike Flynn <spikeon@gmail.com>
2017-06-27 15:31:54 -0700

    Oh yeah...

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Include - 3.4.72
+Include - 3.4.73
 ===
 
 The Include plugin for WordPress allows you to include the content of one page, post, or other such WordPress object into any other with a shortcode.
--- src/plugin.class.php
+++ src/plugin.class.php
@@ -74,9 +74,7 @@ class Plugin extends \PluginFramework\V_1_1\Core {
 
 		$i = new Single($a['id'] ?: $a['slug'] ?: $content, $this->atts('include', $a), $this);
 
-		if(!$this->activate($i->id)) $i->fail();
-
-		return $this->render('include', $i);
+		return $this->render('include', $i );
 
 	}
 
@@ -110,8 +108,6 @@ class Plugin extends \PluginFramework\V_1_1\Core {
 	function shortcode_include_children ($a, $content){
 
 		$i = new Multiple($a['id'] ?: $a['slug'] ?: $content, $this->atts('include_children', $a) , $this);
-
-		if(!$this->activate($i->id)) $i->fail();
 
 		return $this->render('multiple', $i);
 	}
