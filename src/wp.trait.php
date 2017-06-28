140da7a46128214d0402f2109e6114f53c1865c1
Mike Flynn <spikeon@gmail.com>
2017-06-28 11:42:10 -0700

    Removing internal classes for all wp globals

--- src/wp.trait.php
+++ src/wp.trait.php
@@ -3,30 +3,14 @@ namespace IncludePlugin;
 trait WP {
 	public $query_stash;
 
-	public function &wpdb($set = false){
-		global $wpdb;
-		if($set) $wpdb = $set;
-		return $wpdb;
-	}
-
-	public function &wp_query($set = false){
-		global $wp_query;
-		if($set) $wp_query = $set;
-		return $wp_query;
-	}
-
-	public function &post($set = false){
-		global $post;
-		if($set) $post = $set;
-		return $post;
-	}
-
 	public function find_post_type($id){
-		return $this->wpdb()->get_var("SELECT post_type FROM {$this->wpdb()->posts} WHERE ID = '{$id}'") ?: false;
+		global $wpdb;
+		return $wpdb->get_var("SELECT post_type FROM {$wpdb->posts} WHERE ID = '{$id}'") ?: false;
 	}
 
 	public function check_id($id){
-		return $this->wpdb()->get_var("SELECT count(*) FROM {$this->wpdb()->posts} WHERE ID = '{$id}'") ? true : false;
+		global $wpdb;
+		return $wpdb->get_var("SELECT count(*) FROM {$wpdb->posts} WHERE ID = '{$id}'") ? true : false;
 	}
 
 	public function find_id ($subject) {
@@ -36,23 +20,27 @@ trait WP {
 			return $subject * 1;
 		}
 		else {
-			return $this->wpdb()->get_var("SELECT ID FROM {$this->wpdb()->posts} WHERE post_name = '{$subject}'") ?: false;
+			global $wpdb;
+			return $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_name = '{$subject}'") ?: false;
 		}
 	}
 
 	public function find_slug($id) {
-		return $this->wpdb()->get_var("SELECT post_name FROM {$this->wpdb()->posts} WHERE ID = '{$id}'")?: false;
+		global $wpdb;
+		return $wpdb->get_var("SELECT post_name FROM {$wpdb->posts} WHERE ID = '{$id}'")?: false;
 	}
 
 	public function load_wp_query($q) {
-		$this->query_stash = clone $this->wpdb();
+		global $wpdb;
+		$this->query_stash = clone $wpdb;
 		query_posts($q);
 		the_post();
 	}
 
 	public function unload_wp_query() {
-		$this->wpdb(clone $this->query_stash);
-		setup_postdata($this->post());
+		global $wpdb, $post;
+		$wpdb = clone $this->query_stash;
+		setup_postdata($post);
 	}
 
 }
