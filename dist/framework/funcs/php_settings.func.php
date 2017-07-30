3034a72b5734c20ec11e512afbb72b76452d84ee
Mike Flynn <spikeon@gmail.com>
2017-07-29 23:13:24 -0700

    Fixed an issue with multiple versions of plugin framework in same enviornment

--- dist/framework/funcs/php_settings.func.php
+++ dist/framework/funcs/php_settings.func.php
@@ -3,11 +3,11 @@ namespace PluginFramework\V_1_1;
 
 if(empty($GLOBALS['plugin_framework_v_1_1_php_settings'])) $GLOBALS['plugin_framework_v_1_1_php_settings'] = array('allow_url_fopen');
 
-if(!function_exists(__NAMESPACE__ . "\php_settings")){
+if(!function_exists(__NAMESPACE__ . '\php_settings')){
 	function php_settings(){
 
 		// Attempt to tell server to allow url fopen
-		ini_set("allow_url_fopen", 1);
+		ini_set('allow_url_fopen', 1);
 
 		if(ini_get('allow_url_fopen') != 1) return false; // Failed to change setting
 		return true;
--- dist/framework/funcs/register.func.php
+++ dist/framework/funcs/register.func.php
@@ -1,7 +1,7 @@
 <?php
 namespace PluginFramework\V_1_1;
 
-if(! function_exists(__NAMESPACE__ . "\register")) {
+if(! function_exists(__NAMESPACE__ . '\register')) {
 	function register($name, $file){
 		if(empty($GLOBALS['plugin_framework_v_1_1_plugins'])) $GLOBALS['plugin_framework_v_1_1_plugins'] = array();
 		$GLOBALS['plugin_framework_v_1_1_plugins'][$name] = $file;
